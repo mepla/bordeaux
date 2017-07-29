ae8d57227851156b29c9d10033668fc624ddf9dd
Mepla <soheil.nasirian@gmail.com>
2017-07-29 23:37:49 +0430

    Improved .sh for dk_add_link

--- dk_add_link.sh
+++ dk_add_link.sh
@@ -4,3 +4,11 @@ SCRIPT_PATH=/home/mepla/projects/bordeaux/dk_link_manager.py
 source /home/mepla/projects/virtualenvs/bordeaux/bin/activate
 
 python "$SCRIPT_PATH" "$@"
+
+read -p "Do you want to run bordeaux now? (Y/n)" yn
+yn=${yn:-y}
+if [ $yn == 'n' ] || [ $yn == 'N' ]; then
+    exit 0
+else
+    bordeaux
+fi
